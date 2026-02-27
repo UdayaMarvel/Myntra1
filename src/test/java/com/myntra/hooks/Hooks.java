19e5b79391dc9113facdb3ebe1c2f7c6c09523ad
UdayaMarvel <udayamarvel@gmail.com>
2026-02-27 12:32:34 +0530

    Try one more time

--- src/test/java/com/myntra/hooks/Hooks.java
+++ src/test/java/com/myntra/hooks/Hooks.java
@@ -9,20 +9,22 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
-public class Hooks {
-	BaseClass base = new BaseClass();
+public class Hooks extends BaseClass{
+//	BaseClass base = new BaseClass();
 	
 	@Before
 	public void initialisation(Scenario scenario) {
-		final byte[] start = ((TakesScreenshot)base.driver).getScreenshotAs(OutputType.BYTES);
+		browserLaunch(); //launching the browser with object
+		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
 	}
 	
 	
 	@After
 	public void closeInstance(Scenario scenario) {
-		final byte[] start = ((TakesScreenshot)base.driver).getScreenshotAs(OutputType.BYTES);
+		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
+		browserClose(); //closing the browser with object
 	}
 
 }
