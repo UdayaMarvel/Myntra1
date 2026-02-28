3485a46aaae15912820a9fd3e44e21bcd0dc0c0b
gokila1012 <gokilamailu@gmail.com>
2026-02-28 10:35:47 +0530

    new conflict

--- src/test/java/com/myntra/hooks/Hooks.java
+++ src/test/java/com/myntra/hooks/Hooks.java
@@ -9,22 +9,24 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
-public class Hooks extends BaseClass{
-	// BaseClass base = new BaseClass();
+public class Hooks{
+	BaseClass base = new BaseClass();
 	
 	@Before
 	public void initialisation(Scenario scenario) {
-		browserLaunch(); //launching the browser with object
-		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+		base.browserLaunch(); //launching the browser with object
+		
+		final byte[] start = ((TakesScreenshot)base.driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
 	}
 	
 	
 	@After
 	public void closeInstance(Scenario scenario) {
-		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+		final byte[] start = ((TakesScreenshot)base.driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
-		browserClose(); //closing the browser with object
+		
+		base.browserClose(); //closing the browser with object
 	}
 
 }
