869218cbaebf39e3df7517161dd8cda2b23d0976
Subathra-1927 <subathramanibharathi@gmail.com>
2026-03-04 14:32:11 +0530

    Update Hooks.java

--- src/test/java/com/myntra/hooks/Hooks.java
+++ src/test/java/com/myntra/hooks/Hooks.java
@@ -2,7 +2,6 @@ package com.myntra.hooks;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 
 import com.myntra.base.BaseClass;
 
@@ -10,23 +9,22 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
-public class Hooks {
-	WebDriver driver;
+public class Hooks extends BaseClass {
 	 BaseClass base = new BaseClass();
+	String value="Subathra";
 	
 	@Before
 	public void initialisation(Scenario scenario) {
-		base.browserLaunch(); 
+		browserLaunch(); //launching the browser with object
 		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
 	}
+	
 	@After
 	public void closeInstance(Scenario scenario) {
 		final byte[] start = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 		scenario.attach(start, "image/png", "BrowserStart");
-		base.browserClose(); //closing the browser with object
-		
-		//browserClose();
+		browserClose(); //closing the browser with object
 	}
 
 }
